37ef3e715ca17af923405a1e64486b2865b91d31
seongjun_kim <lewisel@naver.com>
2022-02-03 12:11:41 +0900

    feat: Tab 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import Styled from 'styled-components';
 import NavBar from './component/NavBar';
 import Slider from './component/Slider';
+import Tab from './component/Tab';
+import colors from './lib/colors';
 
-
+const Container = Styled.div`
+  height: 100vh;
+  background-color: ${colors.white};
+`
 function App() {
   return (
-    <>
-      <NavBar />
-      <Slider />
-    </>
+    <Container>
+      {/* <NavBar />
+      <Slider /> */}
+      <Tab />
+    </Container>
   );
 }
 
---
+++ src/component/Tab.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import Styled from 'styled-components'
+import colors from '../lib/colors';
+
+const NUM_OF_TABS = 3;
+
+const Container = Styled.div`
+    flex-direction: row;
+    height: 200px;
+    background-color: ${colors.white};
+`
+const TabBar = Styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    height: 80px;
+    background-color: ${colors.gray};
+    padding-left: 20px;
+`
+const TabBox = Styled.button`
+    display:flex;    
+    justify-content: center;
+    align-items: center;
+    width: ${(100 / NUM_OF_TABS)}%;
+    height: 100%;
+    background-color: ${props => props.selected ? colors.primary : colors.gray};
+    color: white;
+    font-weight: bold;
+    text-align: center;
+    transition: background 0.1s linear;
+`
+const Content = Styled.div`
+    display:flex;    
+    justify-content: center;
+    align-items: center;
+    height: 100px;
+    margin: 20px 0px;
+    background-color: ${colors.gray};
+    color: gray;
+    font-weight: bold;
+    text-align: center;
+`
+
+const Tab = () => {
+    const [selectedIndex, setSelectedIndex] = useState(0)
+
+    const handleClick = (index) => {
+        setSelectedIndex(index);
+    }
+
+    const getDisplayText = () => {
+        const statement = 'Tab menu ';
+        switch (selectedIndex) {
+            case 0:
+                return statement + 'ONE';
+            case 1:
+                return statement + 'TWO';
+            case 2:
+                return statement + 'THREE';
+            default:
+                return statement + 'UNKNOWN';
+        }
+    }
+
+    return (
+        <Container>
+            <TabBar>
+                <TabBox selected={selectedIndex === 0} onClick={() => handleClick(0)}>Tab1</TabBox>
+                <TabBox selected={selectedIndex === 1} onClick={() => handleClick(1)}>Tab2</TabBox>
+                <TabBox selected={selectedIndex === 2} onClick={() => handleClick(2)}>Tab3</TabBox>
+            </TabBar>
+            <Content>{getDisplayText()}</Content>
+        </Container>
+    )
+}
+
+export default Tab
